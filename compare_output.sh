5955704f5efd5654586402b1b073a2ec20124f44
Nale Raphael <gmccntwxy@gmail.com>
2025-04-13 20:30:35 +0800

    MAINT: rewrite the approach to get output from command for comparison
    In "compare_output.sh", the original approach for getting output from
    command would fail after Ch5 (include). Output is still visibile in
    terminal if "toyc-chX" is run directly, but it would not be captured
    in the neovim's diff view via bash's process substitution.
    
    So we change the approach to capture command output into redirecting
    both STDOUR and STDERR to a temporary file for now.

--- compare_output.sh
+++ compare_output.sh
@@ -1,22 +1,48 @@
 #!/usr/bin/env bash
 set -eu
 
-ChN=2
+ChN=7
 
 OUR_BIN=./zig-out/bin/toyc-ch${ChN}
 TOY_BIN=./toy_bin/toyc-ch${ChN}
 
+DIR_TMP=/tmp
+OUR_OUT=${DIR_TMP}/result_our.txt
+TOY_OUT=${DIR_TMP}/result_toy.txt
+
 DATA_DIR=./toy_examples/Ch${ChN}
-DATA_NAME=codegen.toy
+DATA_NAME=struct-ast.toy
 INPUT_FILE=${DATA_DIR}/${DATA_NAME}
 
-OTHER_ARGS=( '--emit=mlir' )
+OTHER_ARGS=(
+    '--opt=true'
+    # '--emit=llvm'
+    '--mlir-print-stacktrace-on-diagnostic=true'
+    '--mlir-print-op-on-diagnostic=true'
+    '--mlir-print-op-generic=false'
+    '--mlir-print-ir-before-all=true'
+    '--mlir-print-ir-after-all=true'
+    '--mlir-disable-threading=true'
+)
+
+OUR_ARGS=(
+    '--emit=mlir_llvm'
+)
+TOY_ARGS=(
+    '--emit=mlir-llvm'
+)
 
 if [[ ! -f ${INPUT_FILE} ]]; then
     echo "[ERROR] failed to find input file"
     exit 1
 fi
 
-nvim -d <( ${OUR_BIN} ${INPUT_FILE} ${OTHER_ARGS[@]} 2>&1 ) \
-    <( ${TOY_BIN} ${INPUT_FILE} ${OTHER_ARGS[@]} 2>&1 )
+if [[ ! -d ${DIR_TMP} ]]; then
+    echo "[ERROR] DIR_TMP does not exist: ${DIR_TMP}"
+    exit 1
+fi
 
+${OUR_BIN} ${INPUT_FILE} ${OTHER_ARGS[@]} ${OUR_ARGS[@]} > ${OUR_OUT} 2>&1
+${TOY_BIN} ${INPUT_FILE} ${OTHER_ARGS[@]} ${TOY_ARGS[@]} > ${TOY_OUT} 2>&1
+
+nvim -d ${OUR_OUT} ${TOY_OUT}
